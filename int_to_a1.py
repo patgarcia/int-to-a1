0df7efec145e13adfe2de5d13b2573775cf05b3c
Patricio <patgarcia@users.noreply.github.com>
2021-11-03 23:39:00 -0400

    Docstring and example

--- int_to_a1.py
+++ int_to_a1.py
@@ -1,4 +1,13 @@
+from string import ascii_uppercase
+
 def int_to_a1(num, alphabet):
+    '''
+    Converts column position integer
+    to correponding A1 notation letter.
+    Expects alphabet to be the ASCII 26
+    lowercase or uppercase letters.
+    1 indexed. 0 returns empty string.
+    '''
     base = len(alphabet)
     result = ""
     while num:
@@ -6,10 +15,17 @@ def int_to_a1(num, alphabet):
         while True:
             divisor = base ** exponent
             int_quotient = num // divisor
-            if int_quotient and num - divisor * 26 > 0:
+            if int_quotient and num - divisor * base > 0:
                 exponent += 1
             else:
                 break
         result += alphabet[int_quotient - 1]
         num -= int_quotient * divisor
     return result
+
+
+# EXAMPLE
+# =======
+# num_to_a1(3000, ascii_uppercase) => 'DKJ'
+# num_to_a1(3000, ascii_uppercase) => 'DKJ'
+# num_to_a1(3000, ascii_uppercase) => 'DKJ'
